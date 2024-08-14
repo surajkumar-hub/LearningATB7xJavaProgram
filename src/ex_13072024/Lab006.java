3cc0df6203abd49a26bccf1f1824c84e576262bd
Suraj <surajkulenka@gmail.com>
2024-08-14 09:12:22 +0530

    All source code are added

--- src/ex_13072024/Lab006.java
+++ src/ex_13072024/Lab006.java
@@ -7,33 +7,33 @@ public class Lab006 {
         // data_type variable_name = variable_value(literal)
 
         // Data Type:
-        // Primitive Data Type:
-          // Boolean
+        // 1. Primitive Data Type:
+          // 1.1 Boolean
             // True or False
         boolean is_married = true;
         boolean are_you_a_dog = false;
         System.out.println(is_married);
         System.out.println(are_you_a_dog);
-          // Numerical
-            // Integer -  byte, short, int, long
+          // 1.2 Numerical
+            // 1.2.1 Integer -  byte, short, int, long
         byte b = 100;
         short s = 101;
         int age = 130;
         long phone_number = 7894797919L; //7894797919l;
 
-            // Char - It can store only characters (a)
+            // 1.3 Char - It can store only characters (a)
         char grade = 'a';
 
-            // Floating
-               // Float
+            // 1.4 Floating
+               // 1.4.1 Float
         float pi = 3.14F; //3.14f;
-               // Double
+               // 1.4.2 Double
         double d = 118.654;
 
-        // Non-Primitive Data Type:
+        // 2. Non-Primitive Data Type:
         //Non-primitive means, it's created from a bunch of characters
         // It's created from primitive
-        String name = "Suraj"; // In this case collection of primitive is no-primitive
+        String name = "Suraj"; // In this case collection of primitive is non-primitive
         String name2 = "S";
         //char cc = "c";
     }
--- src/ex_28072024/Lab142.java
+++ src/ex_28072024/Lab142.java
@@ -9,7 +9,7 @@ public class Lab142 {
         dog2Ref.name = "german shepherd";
 
         // What is functions - Function is use to perform a task , block of code
-        // What is Methods - functions within the class are called
+        // What is Methods - functions within the class are called methods
         // What is Class -> It's a blueprint
         // What is Object ->  Real entity created from the class / instance of the class is also called
         // an Object
